642ce4f9bb97989caf18f50632fd07e04a599b80
Egor Letov <trueegorletov@protonmail.com>
2022-07-18 14:29:00 +0300

    Remove the Neovim config

--- nvim/init.lua
+++
@@ -1,8 +0,0 @@
---
---
--- Trick: username wrapper package is used to avoid conflicts and be able to
--- get  proper configuration autoreload without tricks and deals with the devil
---
---
-
-require('trueegorletov.init')
